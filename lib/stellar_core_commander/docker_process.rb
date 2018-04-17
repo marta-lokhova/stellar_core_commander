f3693240503d5c512cfdefd96ed1b16f224914e0
marta-lokhova <marta@stellar.org>
2018-04-17 11:35:31 -0700

    Update uptime/idle time representation

--- lib/stellar_core_commander/docker_process.rb
+++ lib/stellar_core_commander/docker_process.rb
@@ -144,15 +144,15 @@ module StellarCoreCommander
     Contract None => Any
     def dump_system_stats
       fname = "#{working_dir}/../proc-stats-#{Time.now.to_i}-#{rand 100000}.sql"
-      uptime = Time.now - IO.read("/proc/uptime").split[0].to_f
-      idletime = Time.now - IO.read("/proc/uptime").split[1].to_f
+      uptime = IO.read("/proc/uptime").split[0]
+      idletime = IO.read("/proc/uptime").split[1]
 
       meminfo = IO.read("/proc/meminfo")
       netstat = IO.read("/proc/net/dev")
       stat = IO.read("/proc/stat")
 
-      File.open(fname, 'w') {|f| f.write("Uptime: #{uptime} \n Idle time: #{idletime} \n
-        Mem Info: #{meminfo} \n Net stat: #{netstat} \n Other stats: #{stat}")}
+      File.open(fname, 'w') {|f| f.write("Uptime: #{uptime} \nIdle time: #{idletime} \n
+        Mem Info: #{meminfo}\n Net stat: #{netstat}\n Other stats: #{stat}")}
     end
 
     Contract None => Any
